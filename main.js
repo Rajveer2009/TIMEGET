a5d4dfbc55c4bcb8d4833a7424ab70d2aad2dddd
Rajveer <rajveerg2009@gmail.com>
2022-06-10 18:51:31 +0530

    Changes to be committed: 	modified: .gitignore

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-function now(){
-    let today = new Date();
-    let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-    let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    let dob = new Date(date);
-    let dobArr = dob.toDateString().split(' ');
-    let dobFormat = dobArr[2] + ' ' + dobArr[1] + ' ' + dobArr[3];
-    let data = { "date": date, "time": time, "stringDate": dobFormat }
-}
+const date = new Date()
+
+let sec = date.getSeconds()
+let min = date.getMinutes()
+let hours = date.getHours()
+
+const time = hours + ":" + min + ":" + sec
+
+console.log(time);
