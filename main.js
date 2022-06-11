121e098bb1a5b0db728b6f5dc76adee71a02180f
Rajveer <rajveerg2009@gmail.com>
2022-06-11 11:44:26 +0530

    Changes to be committed: 	modified:   main.js 	modified:   package.json

--- main.js
+++ main.js
@@ -1,14 +1,19 @@
+let sec = 0
+let min = 0
+let hours = 0
+let time = ""
+
 const date = new Date()
-let sec = date.getSeconds()
-let min = date.getMinutes()
-let hours = date.getHours()
+sec = date.getSeconds()
+min = date.getMinutes()
+hours = date.getHours()
 
 exports.gettime = function() {
     const date = new Date()
 
-    let sec = date.getSeconds()
-    let min = date.getMinutes()
-    let hours = date.getHours()
+    sec = date.getSeconds()
+    min = date.getMinutes()
+    hours = date.getHours()
 
     time = hours + ":" + min + ":" + sec
 
